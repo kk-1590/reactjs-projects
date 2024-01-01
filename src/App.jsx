ea715d2de79ff49f878754fe9ad29248e7576361
kk-1590 <kkhandelwal1590@gmail.com>
2024-01-01 23:59:52 +0530

    completed random color generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import Accordion from './components/accordion'
+import RandomColor from './components/random-color'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Accordion />
+      {/* <Accordion /> */}
+      <RandomColor />
     </>
   )
 }
