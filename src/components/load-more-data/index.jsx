1a9473edd327d06143bc7bf5f26ce8a76e52e41f
kk-1590 <kkhandelwal1590@gmail.com>
2024-01-06 02:20:27 +0530

    setup LoadMoreData component

---
+++ src/components/load-more-data/index.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from "react";
+
+const LoadMoreData = () => {
+
+    const [loading,setLoading] = useState(false);
+    const [products,setProducts] = useState([]);
+    const [count,setCount] = useState(0);
+
+    useEffect(() => {
+        
+    })
+
+  return (
+    <div className="container">
+
+    </div>
+  )
+}
+
+export default LoadMoreData;
