6a7422bd32c866ae2b5011778b6dbd72ec653d09
kk-1590 <kkhandelwal1590@gmail.com>
2024-01-06 02:55:22 +0530

    completed LoadMoreData component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Accordion from './components/accordion'
 import RandomColor from './components/random-color'
 import StarRating from './components/star-rating'
 import ImageSlider from './components/image-slider'
+import LoadMoreData from './components/load-more-data'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,7 +13,8 @@ function App() {
       {/* <Accordion /> */}
       {/* <RandomColor /> */}
       {/* <StarRating noOfStars={10} /> */}
-      <ImageSlider url={'https://picsum.photos/v2/list'} page={'1'} limit={'10'}/>
+      {/* <ImageSlider url={'https://picsum.photos/v2/list'} page={'1'} limit={'10'}/> */}
+      <LoadMoreData />
     </>
   );
 }
--- src/components/load-more-data/index.jsx
+++ src/components/load-more-data/index.jsx
@@ -1,20 +1,56 @@
 import { useEffect, useState } from "react";
+import "./styles.css";
 
 const LoadMoreData = () => {
+  const [loading, setLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+  const [count, setCount] = useState(0);
+  const [disableButton,setDisableButton] = useState(false);
 
-    const [loading,setLoading] = useState(false);
-    const [products,setProducts] = useState([]);
-    const [count,setCount] = useState(0);
+  useEffect(() => {
+    setLoading(true);
+    async function fetchProducts() {
+      const response = await fetch(
+        `https://dummyjson.com/products?limit=20&skip=${
+          count === 0 ? 0 : count * 20
+        }`
+      );
+      const data = await response.json();
+      console.log(data);
+      console.log(data.products);
+      setLoading(false);
+      setProducts([...products,...data.products]);
+    }
+    fetchProducts();
+  }, [count]);
 
-    useEffect(() => {
-        
-    })
+  useEffect(() => {
+    if(products && products.length === 100){
+        setDisableButton(true);
+    }
+  })
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <div className="container">
-
+      {products.map((item) => (
+        <div key={item.id} className="product">
+          <img
+            src={item.thumbnail}
+            alt={item.title}
+            style={{ height: "100%", width: "100%" }}
+          />
+          <h2>{item.title}</h2>
+        </div>
+      ))}
+      <div>
+        <button disabled={disableButton} onClick={() => setCount(count + 1)}>Load More</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default LoadMoreData;
