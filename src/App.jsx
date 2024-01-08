35f46dce7183b9b0dbf7c19ac5738535d8cda62a
kk-1590 <kkhandelwal1590@gmail.com>
2024-01-08 06:15:10 +0530

    setup TreeView component

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
-import { useState } from 'react'
-import Accordion from './components/accordion'
-import RandomColor from './components/random-color'
-import StarRating from './components/star-rating'
 import ImageSlider from './components/image-slider'
-import LoadMoreData from './components/load-more-data'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       {/* <Accordion /> */}
       {/* <RandomColor /> */}
       {/* <StarRating noOfStars={10} /> */}
-      {/* <ImageSlider url={'https://picsum.photos/v2/list'} page={'1'} limit={'10'}/> */}
-      <LoadMoreData />
+      <ImageSlider url={'https://picsum.photos/v2/list'} page={'1'} limit={'10'}/>
+      {/* <LoadMoreData /> */}
     </>
   );
 }
---
+++ src/components/tree-view/index.jsx
@@ -0,0 +1,7 @@
+const TreeView = ({menus = []}) => {
+  return (
+    <div>index</div>
+  )
+}
+
+export default TreeView;
